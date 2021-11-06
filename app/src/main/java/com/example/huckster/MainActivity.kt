f3f7a85decbb8a03b9e54e91aa213972c8dbcbe5
IlyaBorisovDly <76851543+IlyaBorisovDly@users.noreply.github.com>
2021-11-06 21:19:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/huckster/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.huckster
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.huckster.ui.theme.HucksterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HucksterTheme {
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+}
